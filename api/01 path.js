787f4c12ea027e9133f2cd4bc10d8ba12fa04a99
fhl_mt <crossjae@163.com>
2018-05-30 21:01:36 +0800

    update path

--- api/01 path.js
+++ api/01 path.js
@@ -7,13 +7,12 @@ console.log(path);
  * resolve
  * 将多个片段解析为绝对路径
  * 从右往左处理，依次解析
- * 
- * 最左侧如果不是/开头，则不算绝对路径，前面会加上当前工作目录的路径
 */
 path.resolve(); // 当前工作目录路径
 path.resolve('/foo/bar', 'baz'); // 从右到左解析，碰到/表示拼成绝对路径，/foo/bar/baz
 path.resolve('foo/bar', 'baz'); //没有拼成绝对路径，当前目录+/foo/bar/baz
 path.resolve('/foo/bar', '/baz'); // 最右已经是一个绝对路径，/baz
+path.resolve('////foo/bar'); // 会规范化，同normalize /foo/bar
 
 /*
  * normalize
@@ -25,4 +24,13 @@ path.normalize('temp////name///hi'); // temp/name/hi
 
 /*
  * isAbsolute
+ * 判断是否是绝对路径
+ * win和posix系统有差异
+*/
+path.isAbsolute('/baz'); // true
+path.isAbsolute('../baz'); // false
+
+/*
+ * join
+ * 
 */
